ddbb41a2c11fc9782da69059233e4187afea8143
venkat-gvk <gvk496@gmail.com>
2021-09-29 17:43:37 +0530

    Fixed responsive issue

--- src/components/PersonalDetails.jsx
+++ src/components/PersonalDetails.jsx
@@ -9,30 +9,34 @@ export default function PersonalDetails(props) {
   return (
     <div className="w-full flex justify-center items-center">
       <form>
-        <div>
-          <input
-            name="name"
-            value={user.name}
-            placeholder="Name"
-            required
-            onChange={handleChange}
-            className="w-56 md:w-96 h-4 p-4 rounded mb-3 focus:outline-none focus:ring focus:ring-green-200"></input>
+        <div className="lg:grid grid-cols-2 lg:space-x-3">
+          <div>
+            <input
+              name="name"
+              value={user.name}
+              placeholder="Name"
+              required
+              onChange={handleChange}
+              className="md:w-96 h-4 p-4 rounded mb-3 focus:outline-none focus:ring focus:ring-green-200"
+            />
+          </div>
+
+          <div>
+            <input
+              name="email"
+              value={user.email}
+              placeholder="Email"
+              required
+              onChange={handleChange}
+              className="md:w-96 h-4 p-4 rounded mb-3 focus:outline-none focus:ring focus:ring-green-200"
+            />
+          </div>
         </div>
 
-        <div>
-          <input
-            name="email"
-            value={user.email}
-            placeholder="Email"
-            required
-            onChange={handleChange}
-            className="w-56 md:w-96 h-4 p-4 rounded mb-3 focus:outline-none focus:ring focus:ring-green-200"></input>
-        </div>
-
-        <div>
+        <div className="flex justify-center">
           <button
             onClick={handleNext}
-            className="w-full hover:bg-green-700 px-3 py-1 text-center rounded text-white bg-green-500">
+            className="w-full lg:w-[50%] hover:bg-green-700 px-3 py-1 text-center rounded text-white bg-green-500">
             Next
           </button>
         </div>
--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -14,57 +14,64 @@ export default function UserDetails(props) {
   return (
     <div>
       <form>
-        <div>
-          <input
-            name="name"
-            value={user.firstname}
-            placeholder="FirstName"
-            required
-            onChange={handleChange}
-            className="w-56 md:w-96 h-4 p-4 rounded mb-3 focus:outline-none focus:ring focus:ring-green-200"></input>
+        <div className="lg:grid lg:grid-cols-2 lg:space-x-3">
+          <div>
+            <input
+              name="firstname"
+              value={user.firstname}
+              placeholder="FirstName"
+              required
+              onChange={handleChange}
+              className="md:w-96 h-4 p-4 rounded mb-3 focus:outline-none focus:ring focus:ring-green-200"
+            />
+          </div>
+
+          <div>
+            <input
+              name="lastname"
+              value={user.lastname}
+              placeholder="LastName"
+              required
+              onChange={handleChange}
+              className="md:w-96 h-4 p-4 rounded mb-3 focus:outline-none focus:ring focus:ring-green-200"
+            />
+          </div>
+
+          <div>
+            <input
+              name="education"
+              value={user.education}
+              placeholder="Education"
+              required
+              onChange={handleChange}
+              className="lg:relative lg:right-3 md:w-96 h-4 p-4 rounded mb-3 focus:outline-none focus:ring focus:ring-green-200"
+            />
+          </div>
+
+          <div>
+            <input
+              name="country"
+              value={user.country}
+              placeholder="Country"
+              required
+              onChange={handleChange}
+              className="md:w-96 h-4 p-4 rounded mb-3 focus:outline-none focus:ring focus:ring-green-200"
+            />
+          </div>
         </div>
 
-        <div>
-          <input
-            name="name"
-            value={user.lastname}
-            placeholder="LastName"
-            required
-            onChange={handleChange}
-            className="w-56 md:w-96 h-4 p-4 rounded mb-3 focus:outline-none focus:ring focus:ring-green-200"></input>
-        </div>
-
-        <div>
-          <input
-            name="name"
-            value={user.education}
-            placeholder="Education"
-            required
-            onChange={handleChange}
-            className="w-56 md:w-96 h-4 p-4 rounded mb-3 focus:outline-none focus:ring focus:ring-green-200"></input>
-        </div>
-
-        <div>
-          <input
-            name="name"
-            value={user.country}
-            placeholder="Country"
-            required
-            onChange={handleChange}
-            className="w-56 md:w-96 h-4 p-4 rounded mb-3 focus:outline-none focus:ring focus:ring-green-200"></input>
-        </div>
-
-        <div>
+        <div className="flex justify-center">
           <button
             onClick={handleNext}
-            className="w-full hover:bg-green-700 px-3 py-1 text-center rounded text-white bg-green-500 mb-2">
+            className="w-full lg:w-[50%] hover:bg-green-700 px-3 py-1 text-center rounded text-white bg-green-500 mb-2">
             Next
           </button>
         </div>
-        <div>
+
+        <div className="flex justify-center">
           <button
             onClick={handlePrevious}
-            className="w-full hover:bg-purple-700 px-3 py-1 text-center rounded text-white bg-purple-500">
+            className="w-full lg:w-[50%] hover:bg-purple-700 px-3 py-1 text-center rounded text-white bg-purple-500">
             Back
           </button>
         </div>
